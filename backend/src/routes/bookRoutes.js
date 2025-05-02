ef7faf3685f931fa93840745f70a6e457fb3c7a3
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-05-02 23:12:02 +1000

    bookRoutes

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,6 +1,39 @@
 import express from "express";
+import { cloudinary } from "../lib/cloudinary.js";
+import Book from "../models/Book.js";
 
 const router = express.Router();
 
+router.post("/", async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
+
+    if (!image || !title || !caption || !rating) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+
+    // upload the image to cloudinary
+
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
+
+    // save to the database
+
+    const newBook = {
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      //   user: req.user._id,
+    };
+
+    await newBook.save();
+
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log("Error creating book", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 export default router;
