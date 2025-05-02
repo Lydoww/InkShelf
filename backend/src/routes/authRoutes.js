ca016e4837cdc093638cef5def89b37445495732
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-05-02 19:39:06 +1000

    User model + prehook for pwd

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,51 @@
 import express from "express";
+import User from "../models/User.js";
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-  res.send("register");
+  try {
+    const { email, username, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ message: "Username must be at least 3 characters" });
+    }
+
+    // check if user exists
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    // get random avatar
+    const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+
+    // create new user
+    const user = new User({
+      username,
+      email,
+      password,
+      profileImage,
+    });
+
+    await user.save();
+  } catch (error) {}
 });
 
 router.post("/login", async (req, res) => {
