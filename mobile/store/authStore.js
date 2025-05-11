521a3f5833f6cd4b95e5e8943fb56969f9c6f5e1
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-05-11 16:50:11 +1000

    login function added to store

--- mobile/store/authStore.js
+++ mobile/store/authStore.js
@@ -66,4 +66,41 @@ export const useAuthStore = create((set) => ({
       console.log("Error logging out:", error);
     }
   },
+
+  login: async (email, password) => {
+    set({ isLoading: true });
+    try {
+      const response = await fetch(
+        "https://inkshelf.onrender.com/api/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        }
+      );
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.message || "Something went wrong!");
+      }
+      await AsyncStorage.setItem("user", JSON.stringify(data.user));
+      await AsyncStorage.setItem("token", data.token);
+      set({ token: data.token, user: data.user, isLoading: false });
+      return {
+        success: true,
+        message: "Login successful!",
+      };
+    } catch (error) {
+      set({ isLoading: false });
+      return {
+        success: false,
+        message: error.message || "Something went wrong!",
+      };
+    }
+  },
 }));
